1f9405957ec1a639c4efbe1b9cdbe7da1707ba88
axnish27 <itsaanish10@gmail.com>
2024-08-28 12:20:40 +0530

    routes initialized

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +18,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+Route::resource('task', TaskController::class);
+Route::resource('category', CategoryController::class);
 
 require __DIR__.'/auth.php';
